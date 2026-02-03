a9490e0e844d55077558093394a9e351fddbc2af
Jakob Kunze <kunzejakob2@gmail.com>
2026-02-03 22:26:19 +0100

    update dotfiles

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
+
 vim.o.wrap = false -- do not wrap lines at end of file
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
@@ -13,17 +14,33 @@ vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>p', '"+p')
 
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" }, -- fresh theme
-	{ src = "https://github.com/stevearc/oil.nvim" },  -- file system editor - edit like a buffer
-	{ src = "https://github.com/neovim/nvim-lspconfig" }, -- configs for lsps
-	{ src = "https://github.com/echasnovski/mini.pick" }, -- file picker with fuzzy finding: alternative - telescope
+	{ src = "https://github.com/vague2k/vague.nvim" },        -- fresh theme
+	{ src = "https://github.com/stevearc/oil.nvim" },         -- file system editor - edit like a buffer
+	{ src = "https://github.com/neovim/nvim-lspconfig" },     -- configs for lsps
+	{ src = "https://github.com/echasnovski/mini.pick" },     -- file picker with fuzzy finding: alternative - telescope
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" }, -- preview for typst language
-	{ src = "https://github.com/mason-org/mason.nvim" }, -- lsp manager nvim
+	{ src = "https://github.com/mason-org/mason.nvim" },      -- lsp manager nvim
+	{ src = "https://github.com/lewis6991/hover.nvim" },      -- information when cursor is on a word
 })
 -- omni complete -> look further into ctrl+x
+-- ctrl+x to trigger, then ctrl+o - move with ctrl+n, ctrl+p
 
+-- tell nvim to use autocomplete from lsp
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+
+-- require("hover").setup()
 require("mason").setup()
 require("mini.pick").setup()
 require("oil").setup()
@@ -31,7 +48,8 @@ require("oil").setup()
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 vim.keymap.set('n', '<leader>e', ":Oil<CR>")
-vim.lsp.enable({ "emmylua_ls", "clangd" })
+
+vim.lsp.enable({ "lua_ls", "clangd" })
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.cmd("colorscheme vague")
