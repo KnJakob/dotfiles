f607e76be5048c6b690d17d5331423f7d2ab4de5
Jakob Kunze <kunzejakob2@gmail.com>
2026-02-09 20:41:37 +0100

    update dotfiles

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -13,8 +13,10 @@ vim.o.winborder = "rounded"
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set({'n', 'i', 'x','v'}, '<leader>sh', ':split ')
-vim.keymap.set({'n', 'i', 'x','v'}, '<leader>sv', ':vsplit ')
+vim.keymap.set({ 'n', 'x', 'v' }, '<leader>sh', ':split ')
+vim.keymap.set({ 'n', 'x', 'v' }, '<leader>sv', ':vsplit ')
+vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y')
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>p', '"+p')
@@ -26,6 +28,9 @@ vim.pack.add({
 	{ src = "https://github.com/echasnovski/mini.pick" },     -- file picker with fuzzy finding: alternative - telescope
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" }, -- preview for typst language
 	{ src = "https://github.com/mason-org/mason.nvim" },      -- lsp manager nvim
+	{ src = "https://github.com/hrsh7th/nvim-cmp" },          -- completions ui to interact with lsps
+	{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },      -- connects cmp with nvim-lsp manager
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },          -- shows code snippets
 })
 -- omni complete -> look further into ctrl+x
 -- ctrl+x to trigger, then ctrl+o - move with ctrl+n, ctrl+p
@@ -47,11 +52,48 @@ require("mason").setup()
 require("mini.pick").setup()
 require("oil").setup()
 
+-- lsp commands
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 
-vim.lsp.enable({ "lua_ls", "clangd" })
+-- add functionality of lsps to nvim-cmp if it is available
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+local ok, cmp_lsp = pcall(require, "cmp_nvim_lsp")
+if ok then
+	capabilities = cmp_lsp.default_capabilities(capabilities)
+end
+local cmp = require("cmp")
+
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<S-Tab>"] = cmp.mapping.select_prev_item(),
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+	},
+})
+
+vim.lsp.config.pyright = {
+	cmd = { "pyright-langserver", "--stdio" },
+	filetypes = { "python" },
+	root_markers = { ".git", "pyproject.toml", "setup.py" },
+	capabilities = capabilities,
+}
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "python",
+	callback = function()
+		vim.lsp.start({
+			name = "pyright",
+			cmd = { "pyright-langserver", "--stdio" },
+			root_dir = vim.fn.getcwd(),
+		})
+	end,
+})
+
+vim.lsp.enable({ "lua_ls", "clangd", "pyright" })
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.cmd("colorscheme vague")
